5af6d2be795ee15cda6ffb4c994abe6cd3ce6ae4
Florian Nari <floriannari13@gmail.com>
2020-04-18 21:29:26 +0200

    pour la mise en texte des dates

---
+++ projet/src/controleur/ServicesDivers.java
@@ -0,0 +1,49 @@
+package controleur;
+
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class ServicesDivers {
+
+	DateTimeFormatter dateFormatter;
+
+	DateTimeFormatter heureFormatter;
+	
+	DateTimeFormatter dateHeureFormatter;
+
+	public ServicesDivers() {
+		String date = "dd MMMM yyyy";
+		String heure = "HH'h'mm";
+		dateFormatter = DateTimeFormatter.ofPattern(date);
+		heureFormatter = DateTimeFormatter.ofPattern(heure);
+		dateHeureFormatter = DateTimeFormatter.ofPattern(date + " 'à' " + heure);
+	}
+
+	/**
+	 * exemple : 17 avril 2020
+	 */
+	public String toStringDate(TemporalAccessor date) {
+		return toString(dateFormatter, date);
+	}
+
+	/**
+	 * exemple : 11h21
+	 */
+	public String toStringHeure(TemporalAccessor heure) {
+		return toString(heureFormatter, heure);
+	}
+	/**
+	 * exemple : 17 avril 2020 à 11h21
+	 */
+	public String toStringdateHeure(TemporalAccessor date) {
+		return toString(dateHeureFormatter, date);
+	}
+	
+	private static String toString(DateTimeFormatter formatteur, TemporalAccessor date) {
+		return date == null ? null : formatteur.format(date);
+	}
+}
