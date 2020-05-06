16967c539ba8e3fa7dbd41ad8974c4897c2388b9
Florian Nari <floriannari13@gmail.com>
2020-05-06 20:25:21 +0200

    possibilité de créer un nouvel exemplaire

--- projet/src/controleur/implementation/NouvelExemplaireServiceImplement.java
+++ projet/src/controleur/implementation/NouvelExemplaireServiceImplement.java
@@ -1,19 +1,31 @@
 package controleur.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import controleur.DateService;
+import controleur.PersistanceServiceEcriture;
 import modele.Exemplaire;
 import modele.Oeuvre;
+import modele.primaryKeys.ExemplairePK;
 
 public class NouvelExemplaireServiceImplement implements controleur.NouvelExemplaireService {
 
+	@Autowired
+	private PersistanceServiceEcriture persistance;
+	
 	@Override
 	public Exemplaire ajouterExemplaire(String coteOeuvre) {
-		// TODO Auto-generated method stub
-		return null;
+		Oeuvre oeuvre = persistance.getOeuvre(coteOeuvre);
+		if(oeuvre == null) {
+			
+		}
+		return ajouterExemplaire(oeuvre);
 	}
 
 	@Override
 	public Exemplaire ajouterExemplaire(Oeuvre oeuvre) {
-		// TODO Auto-generated method stub
-		return null;
+		Exemplaire exemplaire = new Exemplaire().setId(new ExemplairePK().setOeuvre(oeuvre)).setDateAchat(DateService.getDateTime());
+		persistance.enregistrer(exemplaire);
+		return exemplaire;
 	}
 }
