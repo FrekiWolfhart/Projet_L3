531ec82742438fe7aacc63e4f03fc5ff125fe2a6
Florian Nari <floriannari13@gmail.com>
2020-05-06 15:50:12 +0200

    implémentation du service permettant d'ajouter un adhérent

--- projet/src/controleur/implementation/NouvelAdherentServiceImplement.java
+++ projet/src/controleur/implementation/NouvelAdherentServiceImplement.java
@@ -1,13 +1,20 @@
 package controleur.implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import controleur.NouvelAdherentService;
+import controleur.PersistanceServiceEcriture;
 import modele.Adherent;
 
 public class NouvelAdherentServiceImplement implements NouvelAdherentService {
 
+	@Autowired
+	private PersistanceServiceEcriture persistance;
+	
 	@Override
 	public Adherent ajouterAdherent(String nom, String email) {
-		// TODO Auto-generated method stub
-		return null;
+		Adherent adherent = new Adherent().setNom(nom).setEmail(email);
+		persistance.enregistrer(adherent);
+		return adherent;
 	}
 }
