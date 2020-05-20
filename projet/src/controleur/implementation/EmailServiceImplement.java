3435ab226acbdb8a105107d5a1b198654bd0e16e
Florian Nari <floriannari13@gmail.com>
2020-05-20 13:22:17 +0200

    importation de javamail

--- projet/src/controleur/implementation/EmailServiceImplement.java
+++ projet/src/controleur/implementation/EmailServiceImplement.java
@@ -6,7 +6,6 @@ import java.util.Properties;
 import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -34,17 +33,15 @@ public class EmailServiceImplement implements EmailService {
 
 	@Override
 	public void envoyerEmailRetardataire(Adherent adherent, Collection<Pret> pretsEnRetard) {
-		if(pretsEnRetard != null) {
-		String email = "Cher(e) "+adherent.getNom()+" "+adherent.getPrenom()
-		+ "\n Nous vous envoyer ce mail car il se trouve que vous avez oublié de nous retourner certains livres empruntés à notre bibliothèque."
-		+ "\n Pour rappel, la duréee d'emprunt est de un mois. Voici la liste des livres concernés :";
-		for(Pret pret : pretsEnRetard){
-			email += "\n - "+pret.getExemplaire();
-		}
-		email += "\n Merci de retourner au plus vite ces livres"
-				+ "\n Cordialement"
-				+ "\n Bibliothèque de l'université Aix-Marseille";
-		envoyerEmail(adherent,email);
+		if (pretsEnRetard != null) {
+			String email = "Cher(e) " + adherent.getNom() + " " + adherent.getPrenom()
+					+ "\n Nous vous envoyer ce mail car il se trouve que vous avez oublié de nous retourner certains livres empruntés à notre bibliothèque."
+					+ "\n Pour rappel, la duréee d'emprunt est de un mois. Voici la liste des livres concernés :";
+			for (Pret pret : pretsEnRetard) {
+				email += "\n - " + pret.getExemplaire();
+			}
+			email += "\n Merci de retourner au plus vite ces livres" + "\n Cordialement" + "\n Bibliothèque de l'université Aix-Marseille";
+			envoyerEmail(adherent, email);
 
 		}
 	}
@@ -52,35 +49,35 @@ public class EmailServiceImplement implements EmailService {
 	@Override
 	public void envoyerEmail(Adherent adherent, String contenu) {
 		Properties properties = new Properties();
-	    properties.setProperty("mail.transport.protocol", "smtp");
-	    properties.setProperty("mail.smtp.host", "smtp.gmail.com");
-	    properties.setProperty("mail.smtp.port", "456");
-	    properties.setProperty("mail.smtp.starttls.enable", "true");
-	    Session session = Session.getInstance(properties);
-	    
-	    MimeMessage message = new MimeMessage(session);
-	    try {
-	    message.setText(contenu);
-	    message.setSubject("Retard");
-	    message.setRecipients(Message.RecipientType.TO, adherent.getEmail());
-	    } catch (MessagingException e) {
-	        e.printStackTrace();
-	    }
-	    
-	    Transport transport;
-	    try {
-			transport = session.getTransport("smtp");
-			transport.sendMessage(message, new Address[] { new InternetAddress(adherent.getEmail()) })
+		properties.setProperty("mail.transport.protocol", "smtp");
+		properties.setProperty("mail.smtp.host", "smtp.gmail.com");
+		properties.setProperty("mail.smtp.port", "456");
+		properties.setProperty("mail.smtp.starttls.enable", "true");
+		Session session = Session.getInstance(properties);
+
+		MimeMessage message = new MimeMessage(session);
+		try {
+			message.setText(contenu);
+			message.setSubject("Retard");
+			message.setRecipients(Message.RecipientType.TO, adherent.getEmail());
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		}
-	    finally {
-	        try {
-	            if (transport != null) {
-	                transport.close();
-	            }
-	        } catch (MessagingException e) {
-	            e.printStackTrace();
-	        }
+
+		Transport transport = null;
+		try {
+			transport = session.getTransport("smtp");
+			transport.sendMessage(message, new Address[] { new InternetAddress(adherent.getEmail()) });
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (transport != null) {
+					transport.close();
+				}
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
