9d5267f0cc486461f26a3f904e05a3ffe4bd9a9f
FrekiWolfhart <yoshii13@yahoo.fr>
2020-05-25 14:19:30 +0200

    Retrait du service et du repository d'auteur vu que l'auteur n'existe plus dans la BDD.

--- projet/src/restful/AuteurRepository.java
+++
@@ -1,9 +0,0 @@
-package restful;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import modele.Auteur;
-
-@RepositoryRestResource
-public interface AuteurRepository extends JpaRepository<Auteur, String>{}
